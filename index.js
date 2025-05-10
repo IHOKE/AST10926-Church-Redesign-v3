1ec3b380f2d5644e95dd4b537a255358a2bf760a
Hermina Tealie <ihokeart@gmail.com>
2025-05-10 17:10:46 +0800

    v1-v0.2 s-news

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const contentData = [
         body: '時間 ： 上午10:45 – 下午14:00<br>地點 ： 香港聖公會麥理浩夫人中心 (石籬和宜合道22號) <br><br>西九龍教區婦女團本屆主題為《跨代手牽手•侍主肩並肩》！希望籍著不同活動，走到教區教省各處，認識聖公會的三頭馬車，怎樣將上帝的愛傳給每一個人！並透過團契活動，讓我們與不同年齡層的弟兄姊妹相知、相交，彰顯主基督的愛。 <br><br>本團將於2025年5月10日(星期六)舉行走訪《麥理浩夫人中心》，認識聖公會在葵涌、青衣及荃灣服務范圍最大，層面最廣的社服機構。詳情如下: <br><br>活動內容 ： 中心服務介紹、明陣、葵華健樂中心等服務 - 參觀及體驗、麥理浩餐廳～中式、西式午餐 <br>名額 ： 80人 (每牧區6名 + 2名後備) <br>費用 ： 全免 <br>報名及查詢： 可聯絡貴堂婦女部部長或代表 <br>截止日期 ： 2025年4月28日(星期一)'
     },
     {
-        title: '主設立聖餐日祝聖聖油暨聖品重申牧職委身禮儀',
+        title: '主設立聖餐日祝聖油暨聖品重申牧職委身禮儀',
         img: 'images/index/Eucharist.jpg',
         body: '時間：上午9:30 <br>地點：聖約翰座堂 (香港中環花園道4-8號) <br>教省於本年主設立聖餐日（4月17日）星期四早上9時半於中環聖約翰座堂舉 行「祝聖聖油暨聖品重申牧職委身禮儀」。 教省主教長陳謳明大主教在當日感恩祭中將祝聖教會生活中使用的三種油膏，分別為：慕道聖油、病者聖油及至聖聖油，同時所有教省聖品在崇拜中將重申牧職委身之承諾。 The Revd Prof. Martyn Percy在崇拜中講道。 歡迎所有信徒一同參禮。'
     },
@@ -30,9 +30,14 @@ function showContent(index) {
     const contentBox = document.getElementById('contentBox');
     const content = contentData[index - 1];
     contentBox.innerHTML = `
-        <h3>${content.title}</h3>
+        <h4>
+        	<span class="chi">${content.title}</span>
+			<span class="eng"></span>
+        </h4>
+
         <div class="s2-slide-img text-center"><img src="${content.img}" alt="Image for ${content.title}"></div>
-        <p class="s2-slide-body overflow-y" style="height: 200px; margin-top: 15px;">
+
+        <p class="chi overflow-y" style="height: 200px; margin-top: 15px;">
             ${content.body}
         </p>
     `;
